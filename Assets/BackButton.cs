87a84ade2548b4af5294e942d28b85cff30bc719
MindCookin <santiagobernabegarcia@MacBook-Pro-de-Santiago.local>
2013-10-14 17:37:52 +0200

    Fixed some minor bugs

--- Assets/BackButton.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class BackButton : MonoBehaviour {
-
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Escape)) { Application.Quit(); }
-	}
-}
---
+++ Assets/Scripts/Utils/BackButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackButton : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) { Application.Quit(); }
+	}
+}
